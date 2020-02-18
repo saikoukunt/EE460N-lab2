e25247edebd7a1883c98c655d7d01e1ee52abfc0
Sai Koukuntla <saikoukuntla@Sais-MacBook-Pro.local>
2020-02-17 22:22:17 -0600

    Finished coding

--- lc3bsim2.c
+++ lc3bsim2.c
@@ -1,12 +1,6 @@
 /*
-    Remove all unnecessary lines (including this one) 
-    in this comment.
-    REFER TO THE SUBMISSION INSTRUCTION FOR DETAILS
-
-    Name 1: Full name of the first partner 
-    Name 2: Full name of the second partner
-    UTEID 1: UT EID of the first partner
-    UTEID 2: UT EID of the second partner
+    Name 1: Sai Susheel Koukuntla
+    UTEID 1: sk46579
 */
 
 /***************************************************************/
@@ -22,69 +16,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-/***************************************************************/
-/*                                                             */
-/* Files: isaprogram   LC-3b machine language program file     */
-/*                                                             */
-/***************************************************************/
-
-/***************************************************************/
-/* These are the functions you'll have to write.               */
-/***************************************************************/
-
-void process_instruction();
-
-/***************************************************************/
-/* A couple of useful definitions.                             */
-/***************************************************************/
+//__________________________________ D E F I N I T I O N S ________________________________________
 #define FALSE 0
 #define TRUE  1
 
-/***************************************************************/
-/* Use this to avoid overflowing 16 bits on the bus.           */
-/***************************************************************/
-#define Low16bits(x) ((x) & 0xFFFF)
+#define Low16bits(x)  ((x) & 0xFFFF)
 
-/***************************************************************/
-/* Main memory.                                                */
-/***************************************************************/
-/* MEMORY[A][0] stores the least significant byte of word at word address A
-   MEMORY[A][1] stores the most significant byte of word at word address A 
-*/
+#define get_DR(x)     (((x) & 0x0E00) >> 9)
+#define get_SR1(x)    (((x) & 0x01C0) >> 6)
+#define get_BaseR(x)  (((x) & 0x01C0) >> 6)
+#define get_SR2(x)    ((x) & 0x0007)
+
+#define sext_imm5(x)  (((x) | ((x) >> 4) * 0xFFFFFFE0))
+#define sext_off6(x)  (((x) | ((x) >> 5) * 0xFFFFFFC0))
+#define sext_off9(x)  (((x) | ((x) >> 8) * 0xFFFFFE00)) 
+#define sext_off11(x) (((x) | ((x) >> 9) * 0xFFFFF800)) 
+#define sext_byte(x)  (((x) | ((x) >> 7) * 0xFF00)) //Automatically omits upper 16 bits
 
 #define WORDS_IN_MEM    0x08000 
-int MEMORY[WORDS_IN_MEM][2];
-
-/***************************************************************/
-
-/***************************************************************/
-
-/***************************************************************/
-/* LC-3b State info.                                           */
-/***************************************************************/
 #define LC_3b_REGS 8
-
-int RUN_BIT;	/* run bit */
-
-
 typedef struct System_Latches_Struct{
-
   int PC,		/* program counter */
     N,		/* n condition bit */
     Z,		/* z condition bit */
     P;		/* p condition bit */
   int REGS[LC_3b_REGS]; /* register file. */
 } System_Latches;
-
-/* Data Structure for Latch */
-
 System_Latches CURRENT_LATCHES, NEXT_LATCHES;
 
-/***************************************************************/
-/* A cycle counter.                                            */
-/***************************************************************/
+#define setCC(x){ \
+      NEXT_LATCHES.N = 0;\
+      NEXT_LATCHES.Z = 0;\
+      NEXT_LATCHES.P = 0;\  
+      if(x > 0) NEXT_LATCHES.P = 1;\
+      else if(x == 0) NEXT_LATCHES.Z = 1;\
+      else if(x < 0) NEXT_LATCHES.N = 1;\
+}
+
+enum opcode {
+add = 0x1, and = 0x5, br = 0x0, jmp = 0xC, jsr = 0x4, ldb = 0x2, ldw = 0x6, 
+lea = 0xE, rti = 0x8, shf = 0xD, stb = 0x3, stw = 0x7, trap = 0xF, xor = 0x9
+};
+
+//_____________________________________ G L O B A L S ______________________________________________
+int RUN_BIT;	/* run bit */
+
+/* MEMORY[A][0] stores the least significant byte of word at word address A
+   MEMORY[A][1] stores the most significant byte of word at word address A */
+int MEMORY[WORDS_IN_MEM][2];
 int INSTRUCTION_COUNT;
+
+//__________________________________ P R O T O T Y P E S __________________________________________
+void process_instruction();
 
 /***************************************************************/
 /*                                                             */
@@ -406,16 +391,162 @@ int main(int argc, char *argv[]) {
 
 /***************************************************************/
 
-
-
+/*  function: process_instruction
+  *  
+  *    Process one instruction at a time  
+  *       -Fetch one instruction
+  *       -Decode 
+  *       -Execute
+  *       -Update NEXT_LATCHES
+  */     
 void process_instruction(){
-  /*  function: process_instruction
-   *  
-   *    Process one instruction at a time  
-   *       -Fetch one instruction
-   *       -Decode 
-   *       -Execute
-   *       -Update NEXT_LATCHES
-   */     
+  //Initialize next latches
+  NEXT_LATCHES = CURRENT_LATCHES;
+
+  //Fetch
+  int pc = CURRENT_LATCHES.PC;
+  int instr = MEMORY[pc >> 1][1] << 8 + MEMORY[pc >> 1][0];
+  NEXT_LATCHES.PC += 2;
+
+  //Decode
+  int opcode = instr >> 12;
+
+  //Execute/Update
+  switch(opcode){
+//______________________________________ A D D | A N D | X O R ___________________________________________
+    case add:
+    case and:
+    case xor:
+      int result;
+      int op1 = CURRENT_LATCHES.REGS[get_SR1(instr)]; //Get value of SR1
+      int op2;
+
+      if(instr & 0x0020){ //bit 5 = 1, so op2 = imm5
+        op2 = instr & 0x001F;
+        op2 = sext_imm5(op2); //sign extend
+      }
+      else{ //bit 5 = 0, op2 = sr2
+        op2 = CURRENT_LATCHES.REGS[get_SR2(instr)]; //Get value of SR2
+      }
+
+      //Compute and store result
+      if(opcode == add){
+        result = op1 + op2;
+      }
+      else if(opcode == and){
+        result = op1 & op2;
+      }
+      else{
+        result = op1 ^ op2;
+      }
+      NEXT_LATCHES.REGS[get_DR(instr)] = Low16bits(result); //Store result
+
+      setCC(result);
+      break;
+
+//__________________________________________________ B R __________________________________________________ 
+    case br:
+      bool n = CURRENT_LATCHES.N && (instr & 0x0800);
+      bool z = CURRENT_LATCHES.Z && (instr & 0x0400);
+      bool p = CURRENT_LATCHES.P && (instr & 0x0200);
+
+      if(n|z|p){
+        int offset = sext_off9(instr & 0x01FF) << 1;
+        NEXT_LATCHES.PC += Low16bits(offset);
+      }
+      break;
+
+//_________________________________________________ J M P _________________________________________________
+    case jmp:
+      NEXT_LATCHES.PC = CURRENT_LATCHES.REGS[get_BaseR(instr)];
+      break;
+
+//_________________________________________________ J S R ________________________________________________
+    case jsr:
+      NEXT_LATCHES.REGS[7] = CURRENT_LATCHES.PC;
+      if(instr & 0x0800){
+        int offset = sext_off11(instr & 0x07FF) << 1;
+        NEXT_LATCHES.PC += Low16bits(offset);
+      }
+      else{
+        NEXT_LATCHES.PC = CURRENT_LATCHES.REGS[get_BaseR(instr)];
+      }
+      break;
+
+//_________________________________________________ L D B ________________________________________________
+    case ldb:
+      int result = CURRENT_LATCHES.REGS[get_BaseR(instr)] + sext_off6(instr);
+      result = Low16bits(result); //just to be safe
+      result = MEMORY[result >> 1][result % 2];
+      result = sext_byte(result);
+
+      NEXT_LATCHES.REGS[get_DR(instr)] = result;
+      setCC(result);
+      break; 
+
+//_________________________________________________ L D W ________________________________________________
+    case ldw:
+      int result = CURRENT_LATCHES.REGS[get_BaseR(instr)] + (sext_off6(instr)) << 1;
+      result = Low16bits(result); //just to be safe
+      result = MEMORY[result >> 1][1] << 8 + MEMORY[result >> 1][0];
+
+      NEXT_LATCHES.REGS[get_DR(instr)] = result;
+      setCC(result);
+      break;
+
+//_________________________________________________ L E A _______________________________________________
+    case lea:
+      int offset = sext_off9(instr & 0x01FF) << 1;
+      NEXT_LATCHES.PC += Low16bits(offset);
+      break;
+
+//________________________________________________ S H F ________________________________________________
+    case shf:
+      int result = CURRENT_LATCHES.REGS[get_SR1(instr)];
+      if(instr & 0x0010){ //right shift
+        if(instr & 0x0020){ //arithmetic
+          int sign = result & 0x8000 >> 15;
+          for(int i = 0; i < (instr & 0x000F); i++){
+            result = sign << 15 + result >> 1;
+          }
+        }
+        else{ //logical
+          result = result >> (instr & 0x000F); 
+        }
+      }
+      else{ //left shift
+        result = result << (instr & 0x000F); 
+      }
+
+      NEXT_LATCHES.REGS[get_DR(instr)] = Low16bits(result);
+      setCC(result);
+      break;
+
+//_______________________________________________ S T B _________________________________________________
+      case stb:
+        int data = CURRENT_LATCHES.REGS[get_DR(instr)] & 0x000000FF;
+        int address = CURRENT_LATCHES.REGS[get_BaseR(instr)] + sext_off6((instr & 0x003F));
+        MEMORY[address >> 1][address % 2] = data;
+        break;
+
+ //______________________________________________ S T W _________________________________________________
+      case stw:
+        int data = CURRENT_LATCHES.REGS[get_DR(instr)];
+        int address = CURRENT_LATCHES.REGS[get_BaseR(instr)] + sext_off6((instr & 0x003F)) << 1;
+        MEMORY[address >> 1][1] = data & 0x0000FF00;
+        MEMORY[address >> 1][0] = data & 0x000000FF;
+        break;       
+
+//______________________________________________ T R A P ________________________________________________
+      case trap:
+        NEXT_LATCHES.REGS[7] = CURRENT_LATCHES.PC;
+        int vect = instr & 0x000000FF;
+        NEXT_LATCHES.PC = MEMORY[vect << 1][1] << 8 + MEMORY[vect << 1][0];
+        break;
+
+    default: //RTI(1000), 1010, 1011
+      break;
+  }
+
 
 }
